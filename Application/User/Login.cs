d510a49f0ef60032b0e31f1cf33870aa8c5deafa
JamesDimitrako <dimdimitrako@gmail.com>
2020-04-08 00:58:35 +0300

    Security -> Add User Controller & User Object

--- Application/User/Login.cs
+++ Application/User/Login.cs
@@ -12,7 +12,7 @@ namespace Application.User
 {
     public class Login
     {
-        public class Query : IRequest<AppUser>
+        public class Query : IRequest<User>
         {
             public string Email { get; set; }
             public string Password { get; set; }
@@ -27,7 +27,7 @@ namespace Application.User
             }
         }
 
-        public class Handler : IRequestHandler<Query, AppUser>
+        public class Handler : IRequestHandler<Query, User>
         {
             private readonly UserManager<AppUser> _userManager;
             private readonly SignInManager<AppUser> _signInManager;
@@ -39,7 +39,7 @@ namespace Application.User
             }
 
 
-            public async Task<AppUser> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<User> Handle(Query request, CancellationToken cancellationToken)
             {
                 var user = await _userManager.FindByEmailAsync(request.Email);
 
@@ -48,10 +48,16 @@ namespace Application.User
 
                 var result = await _signInManager.CheckPasswordSignInAsync(user, request.Password, false);
 
-                if (result.Succeeded)
+               if (result.Succeeded)
                 {
                     // TODO: generate token
-                    return user;
+                    return new User
+                    {
+                        DisplayName = user.DisplayName,
+                        Token = "This will be a token",
+                        Username = user.UserName,
+                        Image = null
+                    };
                 }
 
                 throw new RestException(HttpStatusCode.Unauthorized);
