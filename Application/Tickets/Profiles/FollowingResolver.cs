faca0b9080898c7b0f59354a7e5f79ec270614ea
JamesDimitrako <15272980+JamesDimitrako@users.noreply.github.com>
2020-07-07 18:09:25 +0300

    Custom value resolver for a user

---
+++ Application/Tickets/Profiles/FollowingResolver.cs
@@ -0,0 +1,31 @@
+using System.Linq;
+using Application.interfaces;
+using Application.Tickets.Dtos;
+using AutoMapper;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Tickets.Profiles
+{
+    public class FollowingResolver : IValueResolver<UserTicket, AttendeeDto, bool>
+    {
+        private readonly DataContext _context;
+        private readonly IUserAccessor _userAccessor;
+        public FollowingResolver(DataContext context, IUserAccessor userAccessor)
+        {
+            _userAccessor = userAccessor;
+            _context = context;
+        }
+
+        public bool Resolve(UserTicket source, AttendeeDto destination, bool destMember, ResolutionContext context)
+        {
+            var currentUser = _context.Users.SingleOrDefaultAsync(x => x.UserName == _userAccessor.GetCurrentUsername()).Result;
+
+            if (currentUser.Followings.Any(x => x.TargetId == source.AppUserId))
+                return true;
+
+            return false;
+        }
+    }
+}
--- Application/Tickets/Profiles/MappingProfiles.cs
+++ Application/Tickets/Profiles/MappingProfiles.cs
@@ -15,7 +15,9 @@ namespace Application.Tickets.Profiles
                 .ForMember(d => d.DisplayName, o => o.MapFrom(s => s.AppUser.DisplayName))
                 .ForMember(d => d.Image, o => o.MapFrom(s => s.AppUser.Photos.FirstOrDefault(
                     x => x.IsMain
-                ).Url));
+                ).Url))
+                .ForMember(d => d.Following, o => o.MapFrom<FollowingResolver>());
+
         }
     }
 }
